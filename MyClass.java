c5ce9a7bc4f04cd7863625abd4d13efff9d5d29e
manavkatarey <92071605+manavkatarey@users.noreply.github.com>
2022-08-18 19:31:29 -0400

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass{
+    public static void main(String[] args){
+            int x = 3;
+            int y = 2;
+            if (y / x > 0)
+                System.out.print("positive");
+                System.out.print("negative");
+            }
+    }
