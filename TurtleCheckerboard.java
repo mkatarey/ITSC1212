9657b04ebc46e2eded1fe91c914dbe8cd1af4d21
manavkatarey <92071605+manavkatarey@users.noreply.github.com>
2022-08-18 19:31:52 -0400

    Create TurtleCheckerboard.java

---
+++ TurtleCheckerboard.java
@@ -0,0 +1,13 @@
+public class TurtleCheckerboard {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 0; j < 10; j++) {
+            
+            System.out.println(i + " " + j);
+            
+            }
+            
+            }
+    }
+}
